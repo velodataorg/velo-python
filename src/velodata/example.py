8a9e23afe1d36cd28bdfc2839bfbe6bf8089b25a
velodataapp <hello@velodata.app>
2023-10-02 16:02:15 -0400

    simpler example

--- src/velodata/example.py
+++ src/velodata/example.py
@@ -1,35 +1,22 @@
-import time, math, random
+import time, math
 from velodata import lib as velo
 
+# new velo client
 client = velo.client('api_key')
 
-def get_random_future():
-    futures = client.get_futures()
-    random_future = math.floor(random.random() * len(futures))
-    random_future = futures[random_future]
-    print('random future\n', random_future)
-    return random_future
+# get all futures and pick one
+future = client.get_futures()[0] 
 
-def doWork():
-    random_future = get_random_future()
-
-    params = {
+# last 10 minutes in 1 minute resolution
+params = {
       'type': 'futures',
       'columns': ['open_price', 'close_price'],
-      'exchanges': [random_future['exchange']],
-      'products': [random_future['product']],
-      'begin': math.floor(time.time()*1000) - 1000 * 60 * 11, # 10 minutes
-      'end': math.floor(time.time()*1000),
-      'resolution': 1 # 1 minute
+      'exchanges': [future['exchange']],
+      'products': [future['product']],
+      'begin': math.floor(time.time() * 1000) - 1000 * 60 * 11,
+      'end': math.floor(time.time() * 1000),
+      'resolution': 1
     }
     
-    # stream batches
-    batches = client.batch_rows(params)  
-    for df in client.stream_rows(batches):
-        print(df)
-        
-    # get back all in one
-    print(client.get_rows(params))
-    
-
-doWork()
+# returns dataframe
+print(client.get_rows(params)) 
